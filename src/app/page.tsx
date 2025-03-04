5287bb3f41ca4bd8333fdf101126c7590e44aff5
emdmed <enrique.darderes@gmail.com>
2025-03-04 22:37:04 +0100

    upload file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import ImportDialog from "./components/qr/import/importDialog";
 import ExportDialog from "./components/qr/export/exportDialog";
 import {
@@ -10,20 +10,45 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { encryptString } from "@/lib/encryption/browserEncryption";
 
+type EncryptMode = "text" | "file";
+
 export default function Home() {
-  const [toggleShareDialog, setToggleShareDialog] = useState(false);
-  const [encryptedData, setEncryptedData] = useState("");
-  const [secretKey, setSecretKey] = useState("");
-  const [text, setText] = useState("");
+  const [toggleShareDialog, setToggleShareDialog] = useState<boolean>(false);
+  const [encryptedData, setEncryptedData] = useState<string>("");
+  const [secretKey, setSecretKey] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  const [encryptMode, setEncrypMode] = useState<EncryptMode>("text");
+  const [fileContent, setFileContent] = useState<string>("");
+
+  console.log("fileContent", fileContent);
 
   const handleEncryptAction = () => {
-    setEncryptedData(encryptString(text, secretKey));
+    if (encryptMode === "text") {
+      setEncryptedData(encryptString(text, secretKey));
+    }
+    if (encryptMode === "file") {
+      setEncryptedData(encryptString(fileContent, secretKey));
+    }
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        const result = event.target?.result;
+        setFileContent(result as string);
+      };
+      reader.onerror = (err) => {
+        console.error("Error reading file:", err);
+      };
+      reader.readAsText(file, "utf8");
+    }
   };
 
   return (
@@ -35,20 +60,12 @@ export default function Home() {
         <div className="grid w-full">
           <div className="my-2">
             {!toggleShareDialog && (
-              <Button
-                onClick={() => {
-                  setToggleShareDialog(true);
-                }}
-              >
+              <Button onClick={() => setToggleShareDialog(true)}>
                 Go to scan
               </Button>
             )}
             {toggleShareDialog && (
-              <Button
-                onClick={() => {
-                  setToggleShareDialog(false);
-                }}
-              >
+              <Button onClick={() => setToggleShareDialog(false)}>
                 Go to generate Qr code
               </Button>
             )}
@@ -56,20 +73,35 @@ export default function Home() {
           {!toggleShareDialog && (
             <Card className="rounded-none border-4 border-double">
               <CardHeader>
-                <CardTitle>
+                <CardTitle className="flex gap-2 items-center">
                   <span>Encrypt</span>
+                  <Button onClick={() => setEncrypMode("file")} size="sm">
+                    File
+                  </Button>
+                  <Button onClick={() => setEncrypMode("text")} size="sm">
+                    Text
+                  </Button>
                 </CardTitle>
                 <CardDescription>
                   Add the text to encrypt and generate a QR
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <Textarea
-                  className="border-secondary"
-                  value={text}
-                  onChange={(e) => setText(e.target.value)}
-                  placeholder="Secret message..."
-                ></Textarea>
+                {encryptMode === "text" && (
+                  <Textarea
+                    className="border-secondary"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Secret message..."
+                  />
+                )}
+                {encryptMode === "file" && (
+                  <Input
+                    className="border-secondary"
+                    type="file"
+                    onChange={handleFileChange}
+                  />
+                )}
                 <div className="flex justify-end py-2 gap-2">
                   <Input
                     className="border-secondary"
@@ -80,7 +112,10 @@ export default function Home() {
                   />
                   <Button
                     onClick={handleEncryptAction}
-                    disabled={!(secretKey && text)}
+                    disabled={
+                      !secretKey ||
+                      (encryptMode === "text" ? !text : !fileContent)
+                    }
                   >
                     Encrypt
                   </Button>
