8c4a860b7f053035bcd3978293d31346aea0c25c
emdmed <enrique.darderes@gmail.com>
2025-03-13 22:56:37 +0100

    disable unused for qrCodeImporter file

--- src/app/components/qr/import/qrCodeImporter.js
+++ src/app/components/qr/import/qrCodeImporter.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from 'react';
 import { Scanner } from '@yudiel/react-qr-scanner';
 import { Button } from '@/components/ui/button';
