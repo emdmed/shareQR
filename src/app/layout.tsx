ce3fb59d310cba68b215f8a222266849e2b8b908
enrique <enrique.darderes@gmail.com>
2025-11-01 12:16:21 +0100

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased screen `}
       >
         {children}
       </body>
